cbd0e6c896bbfa4c17852bf853d2bec6abef0bb9
md-sabith <258sabithp@gmail.com>
2025-09-14 17:31:29 +0530

    added web url to home as const

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header/Header'
 import AllClass from '../components/allClasses/AllClass'
 
 function Home() {
+  const WebURL="https://clg-project-nu.vercel.app/"
   return (
     <div>
         <Header/>
